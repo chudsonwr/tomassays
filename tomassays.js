77b4c651e0a1475a7d07b5f7ea8af8a793813b74
chudsonwr <chudson@worldremit.com>
2018-01-24 20:55:04 +0000

    Update tomassays.js

--- tomassays.js
+++ tomassays.js
@@ -28,5 +28,5 @@ http.createServer(function (req, res) {
             res.end(content);
         }
     });
-}).listen(8080);
+}).listen(80);
 console.log("Server running at http://localhost:8080/");
