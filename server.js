cf6669713df583410dc1929f8e9ef23e649cb4d7
Chris Hudson <chudson@worldremit.com>
2018-01-25 16:19:23 +0000

    updates and smaller pics

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ var http = require('http'),
     fs = require('fs'),
     url = require('url');
  
+
+
 //create http server listening on port 3333
 http.createServer(function (req, res) {
     //use the url to parse the requested url and get the image name
     var query = url.parse(req.url,true).query;
+    if (query.token != "igA5lsULEC6eVhhoiyW7KaQ4") {
+        res.writeHead(403, {'Content-type':'text/html'})
+        console.log(query.token)
+        res.end("wrong token");
+    } else {
+    
         pic = query.image;
- 
 
-    //get items in images folder
-    global.items = []
-    fs.readdirSync('./images/').forEach(file => {
-        items.push(file);
-    }) 
+         global.items = []
+ fs.readdirSync('./images/').forEach(file => {
+    items.push(file);
+ }) 
 
+  if (pic) {  
     //read the image using fs and send the image content back in the response
-    fs.readFile('./images/' + items[Math.floor(Math.random()*items.length)], function (err, content) {
+    fs.readFile('./images/' + pic, function (err, content) {
         if (err) {
             res.writeHead(400, {'Content-type':'text/html'})
             console.log(err);
@@ -28,5 +35,23 @@ http.createServer(function (req, res) {
             res.end(content);
         }
     });
-}).listen(1337);
-console.log("Server running at http://localhost:8080/");
+  } else {
+    
+     res_object = {}
+            arr = []
+            imageattach = {}
+            imageattach["image_url"] = "https://jenkins.csre.worldremit.com:1337/?token=igA5lsULEC6eVhhoiyW7KaQ4&image=" + items[Math.floor(Math.random()*items.length)]
+            arr.push(imageattach)
+            res_object['attachments'] = arr
+            res_object['text'] = "Tomas Says....."
+            res_object['parse'] = 'full'
+            res_object['response_type'] = 'in_channel'
+            res_object['unfurl_media'] = true
+            res_object['unfurl_links'] = true
+            res.writeHead(200,{'Content-type':'application/json'});
+            res.end(JSON.stringify(res_object));
+
+  }
+
+}}).listen(1338);
+console.log("Server running at http://localhost:1338/");
